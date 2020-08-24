5f079b25fa900d902f2d07b8d3029bd9a9396bd6
unknown <GradGradius>
2020-08-24 09:10:02 +0100

    added login

--- datagen/data-generator-master/main.py
+++ datagen/data-generator-master/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
+from flask_mysqldb import MySQL
 from flask_cors import CORS
 import webServiceStream
 from RandomDealData import *
@@ -7,8 +8,15 @@ import mysql.connector
 app = Flask(__name__)
 CORS(app)
 
-cnx = mysql.connector.connect(host='localhost', database='db_grad_cs_1917', user='root',password='ppp', port='3306')
+#cnx = mysql.connector.connect(host='localhost', database='db_grad_cs_1917', user='root',password='ppp', port='3306')
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'ppp'
+app.config['MYSQL_DB'] = 'db_grad_cs_1917'
+app.config['MYSQL_DATABASE_PORT'] = '3306'
+
+mysql = MySQL(app)
 
 
 @app.route('/')
@@ -55,6 +63,24 @@ def anonymous_users_post():
     cnx.commit()
     cnx.close()
     return 'success'
+
+@app.route('/login')
+def login():
+
+    cursor = mysql.connection.cursor()
+    query = ("SELECT * FROM anonymous_users")
+    data = {
+        "anonymous_user_id" : "1234567",
+        "anonymous_user_pwd": "1234567"
+    }
+    cursor.execute(query)
+    mysql.connection.commit()
+    r = [dict((cursor.description[i][0], str(value)) for i, value in enumerate(row)) for row in cursor.fetchall()]
+    cursor.close()
+    res = ''
+    for s in r:
+        res += str(s) + '\n'
+    return res
 
 
 def bootapp():
