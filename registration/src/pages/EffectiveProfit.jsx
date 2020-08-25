a9cf1640d9010e59c891caa411af88dfdddcc69c
gradgrad <gradgradius@gmail.com>
2020-08-25 10:00:04 +0100

    dealer

--- registration/src/pages/EffectiveProfit.jsx
+++ registration/src/pages/EffectiveProfit.jsx
@@ -4,9 +4,11 @@ import axios from 'axios';
 import styles from './tablebox.module.css'
 
 const EffectiveProfit = () => {
-    const [data, setData] = useState('');
+    const [data, setData] = useState([]);
+    const [dealer, setDealer] = useState(701)
     const creds = {
-        "user_id": exportedUserName
+        "user_id": exportedUserName,
+        "delaer_id": dealer
     }
     
     async function handleClick() {
@@ -20,6 +22,14 @@ const EffectiveProfit = () => {
     return (
         <div className={styles.tablebox}>
         <button className="pure-button" onClick={handleClick}>See Effective Profit</button>
+        <label htmlFor="cars">Choose a car:</label>
+
+        <select name="cars" id="cars">
+        <option value="701">701</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+        </select>
         <table className="pure-table">
         <thead>
         <tr>
@@ -29,7 +39,7 @@ const EffectiveProfit = () => {
         </tr>
         </thead>
         <tbody>
-        {/* {data.map(el => {
+        { {data.map(el => {
             return(
             <tr>
                 <th>{el.dealer_id}</th>
@@ -37,7 +47,7 @@ const EffectiveProfit = () => {
                 <th>{el.dealer_instrument_id}</th>
             </tr>
             )
-        })} */}
+        })} }
         </tbody>
         </table>
         </div>
